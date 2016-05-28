59fd79d7a428e62fd11e305afc3df0c56a3ec8b3
jmoy-wcs <jmoy@wcs.org>
2016-05-28 17:54:57 -0400

    first commit

---
+++ read_raster_example.py
@@ -0,0 +1,35 @@
+import numpy as np
+from osgeo import gdal
+from osgeo.gdalconst import *
+from osgeo import gdal_array
+import os
+
+ROOT_DIR = r'C:\Users\Jesse Moy\Documents\Python practice\untitled\dem'
+raster_path = 'dem.tif'
+
+# open raster
+raster = gdal.Open(os.path.join(ROOT_DIR, raster_path), GA_ReadOnly)
+
+print type(raster)
+
+# access raster attributes
+gt = raster.GetGeoTransform()
+attributes = {'origin: ': (gt[0], gt[3]),
+              'cell_size': gt[1],
+              'shape': (raster.RasterXSize, raster.RasterYSize),
+              'driver': raster.GetDriver().LongName
+              }
+
+print attributes
+# convert raster object to numpy array
+array = gdal_array.DatasetReadAsArray(raster)
+
+def get_raster_values(array):
+    """
+    # get unique values and counts for categorical raster
+    :param array:
+    :return: value_count_dict
+    """
+    unique = np.unique(array, return_counts=True)
+    value_count_dict = dict(zip(unique[0], unique[1]))
+    return value_count_dict
